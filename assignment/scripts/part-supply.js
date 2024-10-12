fd89c67ecfd724cc20642224ed3b72f26bea81ab
Blair Jilek <blairjilek@gmail.com>
2024-10-12 18:16:09 -0500

    assigned second item from array

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -15,7 +15,7 @@ let supplyChanges = [3, 5, -6, 0, 7, 11];
 // 3. Create a variable called 'secondItem' and assign it the value of the second
 //    item in the 'supplyChanges' array.
 console.log('3. Access the second value of supplyChanges:');
-
+let secondItem = supplyChanges[1];
 
 // 4. The last value in the 'supplyChanges' array was added by mistake.
 //    Remove it from the array and store it inside a new variable called 'removedItem'.
